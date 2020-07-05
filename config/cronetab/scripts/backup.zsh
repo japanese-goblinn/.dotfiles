30484f1105eac0377624da082ca2b569a5c78fd9
japanese-goblinn <cool45akol@gmail.com>
2020-07-05 23:45:06 +0300

    fix: crontab job

--- config/cronetab/scripts/backup.zsh
+++ config/cronetab/scripts/backup.zsh
@@ -1,1 +1,1 @@
-code --list-extensions > ~/.dotfiles/config/vscode/extensions.txt || echo 'Failed to backup'
+/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code --list-extensions > ~/.dotfiles/config/vscode/extensions.txt || echo 'Failed to backup'
