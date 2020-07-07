1c256dc5e1812fc19fddf16875e7191f43485310
japanese-goblinn <cool45akol@gmail.com>
2020-07-07 22:01:31 +0300

    feat(config): iterm auto theme switch

---
+++ config/iterm/auto_dark_mode.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3.7
+
+import asyncio
+import iterm2
+
+async def update(connection, theme):
+    parts = theme.split(" ")
+    if "dark" in parts:
+        preset = await iterm2.ColorPreset.async_get(connection, "Pastel (Dark Background)")
+    else:
+        preset = await iterm2.ColorPreset.async_get(connection, "Night Owlish Light")
+    profiles=await iterm2.PartialProfile.async_query(connection)
+    for partial in profiles:
+        profile = await partial.async_get_full_profile()
+        await profile.async_set_color_preset(preset)
+
+async def main(connection):
+    app = await iterm2.async_get_app(connection)
+    await update(connection, await app.async_get_variable("effectiveTheme"))
+    async with iterm2.VariableMonitor(connection, iterm2.VariableScopes.APP, "effectiveTheme", None) as mon:
+        while True:
+            theme = await mon.async_get()
+            await update(connection, theme)
+
+
+iterm2.run_forever(main)
