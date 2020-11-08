61caa42e4e20956ae1cd028fadce7fdae47f3773
japanese-goblinn <cool45akol@gmail.com>
2020-11-08 16:41:52 +0300

    feat(config/iterm): update theme in script

--- config/iterm/auto_dark_mode.py
+++ config/iterm/auto_dark_mode.py
@@ -3,16 +3,18 @@
 import asyncio
 import iterm2
 
+
 async def update(connection, theme):
     parts = theme.split(" ")
     if "dark" in parts:
         preset = await iterm2.ColorPreset.async_get(connection, "Pastel (Dark Background)")
     else:
-        preset = await iterm2.ColorPreset.async_get(connection, "Night Owlish Light")
-    profiles=await iterm2.PartialProfile.async_query(connection)
+        preset = await iterm2.ColorPreset.async_get(connection, "Light Background")
+    profiles = await iterm2.PartialProfile.async_query(connection)
     for partial in profiles:
         profile = await partial.async_get_full_profile()
         await profile.async_set_color_preset(preset)
+
 
 async def main(connection):
     app = await iterm2.async_get_app(connection)
