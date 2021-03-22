f76aecd344fc2edd53ea864dc66ec392f9c89e6c
japanese-goblinn <cool45akol@gmail.com>
2021-03-22 21:44:29 +0300

    feat: shell update

--- shell/.prompt.zsh
+++ shell/.prompt.zsh
@@ -3,7 +3,7 @@
 # 1. `unlink /usr/local/share/zsh/site-functions/prompt_pure_setup`
 # 2. `ln -s $DOTFILES_DEPENDECIES_PATH/pure/pure.zsh /usr/local/share/zsh/site-functions/prompt_pure_setup`
 
-fpath=("$HOME/.dotfiles/dependencies/pure" $fpath)
+fpath=("$DOTFILES_DEPENDECIES_PATH/pure" $fpath)
 
 autoload -U promptinit; promptinit
 prompt pure
