4f37e317c83cdd283b55d623e80ed1abc5ba427b
japanese-goblinn <cool45akol@gmail.com>
2021-09-02 22:15:08 +0300

    feat(shell): better functions

--- shell/functions.sh
+++ shell/functions.sh
@@ -1,27 +1,30 @@
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BOLD="\033[1m"
+PURPLE="\033[95m"
+RESET="\033[0m"
+
+function _print_error() { echo -e "\n❌ ${BOLD}${RED}${1}${RESET}"; }
+
 ERROR_CODE=1
 
 function to() {
   local file_name="$1"
   if [ -z $file_name ]; then 
-    echo -e "\e[31mFILE NAME SHOULD BE PASSED\e[0m"
+    _print_error "FILE NAME SHOULD BE PASSED"
     return $ERROR_CODE
   fi
   touch $file_name
   e "file_name"
 }
 
-
 function clear_vscode_cache() {
   rm -rf ~/Library/Application\ Support/Code/Cache/*
   rm -rf ~/Library/Application\ Support/Code/CachedData/*
 }
 
-: '
-- Description:
-    This function sets up new repository with everything you need. 
-- Usage: 
-    gi <directory-name> 
-'
+# This function sets up new repository with everything you need.
 function gi() {
   if [[ -n "$1" ]]; then
     mkdir "$1"
@@ -32,90 +35,41 @@ function gi() {
     touch .gitignore
     echo -e ".DS_Store" > .gitignore
   else
-    echo -e "\e[31mDIRECTORY NAME SHOULD BE PASSED\e[0m"
+    _print_error "DIRECTORY NAME SHOULD BE PASSED"
   fi
 }
 
-: '
-- Description:
-    This function acts like rm command, but in safer maner (moves your files and/or directories into Trash).
-- Usage: 
-    t <list-of-files-and/or-directories> (also works if there are only one file/directory)
-'
 function t() {
   if [[ -n "$1" ]]; then
     mv "$@" ~/.Trash
   else
-    echo -e "\e[31m!ERROR!\e[0m At least one file/directory name should be passed"
+    _print_error "At least one file/directory name should be passed"
   fi
 }
 
-: '
-- Description:
-    Move to trash current folder.
-'
 function tc() {
   local folderName=$(pwd)
   cd ../
   t "$folderName"
 }
 
-: '
-- Description:
-    This function copies current directory path in clipboard.
-'
-function cpwd() { 
-  pwd | pbcopy
-}
+function cpwd() {  pwd | pbcopy; }
 
-: '
-- Description:
-    This function copies currently opened page in Safari URL into clipboard.
-'
-function cpsf() { 
-  osascript -e 'tell application "Safari" to return URL of front document' | pbcopy
-}
+# copies currently opened page in Safari URL into clipboard.
+function cpsf() {  osascript -e 'tell application "Safari" to return URL of front document' | pbcopy; }
 
-: '
-- Description:
-    This function creates new file and opens it in micro.
-- Usage:
-    cf <file1.ext> <file2.ext>...
-'
-function cf() {
-  if [[ -n "$1" ]]; then
-    touch "$@"
-    micro "$@"
-  else
-    echo -e "\e[31m!ERROR!\e[0m File name should not be empty"
-  fi
-}
+function mk() {  mkdir -p "$1"; }
 
-function mk() { 
-  mkdir -p "$1" 
-}
-
-: '
-- Description:
-    This function creates directory and then cd into it.
-- Usage:
-    md <directory-name>
-'
 function md() {
-  if [[ -n "$1" ]]; then
-    mkdir -p "$1"
-    cd "$1" || exit
+  local dir="$1"
+  if [[ -n $dir ]]; then
+    mkdir -p $dir
+    cd $dir || exit
   else
-    echo -e "\e[31m!ERROR!\e[0m File name should not be empty"
+    _print_error "DIRECTORY NAME SHOULD NOT BE EMPTY"
   fi
 }
 
-: '
-- Description:
-    Create new python virtual environment and activate it
-- Usage: 
-    venv <optional-venv-directory-name>
-'
 function venv() {
   if [[ -n "$1" ]]; then
     python3 -m venv "$1"
