8a7f379eda0b71778b525d11304747a258f240c1
japanese-goblinn <cool45akol@gmail.com>
2021-09-05 15:58:57 +0300

    refactor(shell): exports

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,10 +1,4 @@
-# path's
-export DOTFILES_DEPENDECIES_PATH="$DOTFILES_PATH/dependencies"
-export DOTFILES_CONFIG_PATH="$DOTFILES_PATH/config"
-export LAZYGIT_CONFIG_PATH="$DOTFILES_CONFIG_PATH/lazygit"
-
-# zsh-autosuggestions
-export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#cf2f98"
+#!/bin/bash
 
 # vim errors fix
 export LC_ALL=en_US.UTF-8
@@ -13,17 +7,31 @@ export LC_ALL=en_US.UTF-8
 export VISUAL=micro
 export MICRO_TRUECOLOR=1
 
+# path's
+export DOTFILES_DEPENDECIES_PATH="$DOTFILES_PATH/dependencies"
+export DOTFILES_CONFIG_PATH="$DOTFILES_PATH/config"
+export LAZYGIT_CONFIG_PATH="$DOTFILES_CONFIG_PATH/lazygit"
+
+# zsh-autosuggestions
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#cf2f98"
+
+# bat
+export MANPAGER="sh -c 'col -bx | bat -l man -p'" # colorized man page
+
+export BAT_LIGHT_THEME="Coldark-Cold"
+export BAT_DARK_THEME="OneHalfDark"
+
 # fzf
 export FZF_DEFAULT_OPTS="
 --layout=reverse
 --info=inline
 --height=80%
 --multi
---preview '([[ -f {} ]] && (bat --theme=\$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo OneHalfDark || echo Coldark-Cold) --style=numbers --color=always --line-range=:500  {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'
+--preview '([[ -f {} ]] && (bat --theme=\$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo $BAT_DARK_THEME || echo $BAT_LIGHT_THEME) --style=numbers --color=always --line-range=:500  {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'
 "
-
-# bat
-export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
 # fuck
 export THEFUCK_NO_COLORS="true"
+
+# homebrew
+export HOMEBREW_NO_ANALYTICS=1  # disables statistics that brew collects
