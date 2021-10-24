3abf8a48768ab93734325b27ee617b9507308895
japanese-goblinn <cool45akol@gmail.com>
2021-10-24 22:43:55 +0300

    refactor(config): defaults and raycast move to config directory

--- config/macos/defaults.sh
+++ config/macos/defaults.sh
@@ -4,6 +4,9 @@ set -e
 
 echo "Configuring macOS defaults..."
 
+# Disable automatically rearrange Spaces based on recent use
+defaults write com.apple.dock mru-spaces -bool false
+
 # Require password immediately after sleep or screen saver begins
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
