5ce01865b367a97d4e9e2a736b5924e6ee9b7286
japanese-goblinn <cool45akol@gmail.com>
2022-04-10 23:38:06 +0300

    feat: setup touch id for sudo script

---
+++ shell/setup_sudo_touch_id.sh
@@ -0,0 +1,18 @@
+SCRIPT_DIR_PATH="$( cd "$(dirname "$0")" && pwd )"
+source "$SCRIPT_DIR_PATH/functions.sh"
+
+if [[ "$(id -u)" != 0 ]]; then
+  print_error "'sudo' is required"
+  exit $ERROR_CODE
+fi
+
+sudo_file="/etc/pam.d/sudo"
+if grep -q "pam_tid.so" "$sudo_file"; then
+  print_warning "Touch ID for 'sudo' already configured"
+else
+  # lines after are aligned this way becouse otherwise lines inserted with spacing at the beggining :c
+  sed -i '' '1a\
+auth       sufficient     pam_tid.so\
+' "$sudo_file"
+  print_success "Touch ID for 'sudo' configured"
+fi
