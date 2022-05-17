b5ffdadc9c30b2d281542f7c381fabf403fcaffc
japanese-goblinn <cool45akol@gmail.com>
2022-05-17 08:54:34 +0300

    fix(shell): sudo script functions update

--- shell/setup_sudo_touch_id.sh
+++ shell/setup_sudo_touch_id.sh
@@ -2,17 +2,17 @@ SCRIPT_DIR_PATH="$( cd "$(dirname "$0")" && pwd )"
 source "$SCRIPT_DIR_PATH/functions.sh"
 
 if [[ "$(id -u)" != 0 ]]; then
-  print_error "'sudo' is required"
+  _print_error "'sudo' is required"
   exit $ERROR_CODE
 fi
 
 sudo_file="/etc/pam.d/sudo"
 if grep -q "pam_tid.so" "$sudo_file"; then
-  print_warning "Touch ID for 'sudo' already configured"
+  _print_warning "Touch ID for 'sudo' already configured"
 else
   # lines after are aligned this way becouse otherwise lines inserted with spacing at the beggining :c
   sed -i '' '1a\
 auth       sufficient     pam_tid.so\
 ' "$sudo_file"
-  print_success "Touch ID for 'sudo' configured"
+  _print_success "Touch ID for 'sudo' configured"
 fi
