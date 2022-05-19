967147c6c9cbded9e342016fbec7c05419f95bbe
japanese-goblinn <cool45akol@gmail.com>
2022-05-19 21:29:15 +0300

    feat(raycast/scripts): basic url encoding and http problem fix

--- config/raycast/scripts/firefox_copy_current_url_as_markdown.sh
+++ config/raycast/scripts/firefox_copy_current_url_as_markdown.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 # @raycast.schemaVersion 1
 # @raycast.icon 🦊
@@ -10,14 +10,24 @@
 # [Getting URL and Tab Title from Firefox with AppleScript · Matt's programming blog](https://matthewbilyeu.com/blog/2018-08-24/getting-url-and-tab-title-from-firefox-with-applescript)
 # [macos - Get URL of opened Firefox tabs from terminal - Ask Different](https://apple.stackexchange.com/questions/404841/get-url-of-opened-firefox-tabs-from-terminal)
 
-set -e
+# only encode ` ` as %20 for now
+function url_encode() {
+    local raw_url
+    raw_url="$1"
+    local url_encoded
+    url_encoded="${raw_url// /%20}"
+    echo "$url_encoded"
+}
 
 LANG="en_US.UTF-8"
 
 name=$( osascript -e 'tell application "System Events" to tell process "Firefox" to get name of front window')
 link=$( osascript -e  'tell application "System Events" to tell process "Firefox" to get value of UI element 1 of combo box 1 of toolbar "Navigation" of first group of front window' )
 
-# TODO: proper url encoding (e.g. if contains spaces encode them with %20)
+# if url is http - firefox copies it in format `path/` instead of `http://path/` and here is a fix
+if [[ ! "$link" =~ "(https)+" ]]; then
+	link="http://$link"
+fi
 
-echo "[$name]($link)" | pbcopy
+echo "[$name]($(url_encode "$link"))" | pbcopy
 echo "Link Copied"
