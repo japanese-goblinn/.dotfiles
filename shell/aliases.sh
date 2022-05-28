011ae398a1593fca55e04350bf7986bb03516975
japanese-goblinn <cool45akol@gmail.com>
2022-05-28 19:29:28 +0300

    refactor(shell/aliases): remove unused

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -8,6 +8,7 @@ alias ls="exa"
 alias ll="exa -la"
 alias c="clear"
 alias o="open"
+alias oc="open ."
 alias diff="code -nd"
 alias reload="source ~/.zshrc"
 alias f="fork"
@@ -19,7 +20,6 @@ alias grep="rg"
 alias b="bat --theme=\$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo $BAT_DARK_THEME || echo $BAT_LIGHT_THEME)"
 
 # Navigation
-alias .="cd -"
 alias ..="cd .."
 alias ...="cd ../../"
 alias dev="cd ~/Developer"
@@ -34,31 +34,19 @@ alias brup="brew update && brew cleanup"
 # git
 alias g="git"
 alias gb="git branch"
-alias gc="git commit"
 alias gcm="git commit -m"
 alias gch="git checkout"
-alias gre="git reset"
 alias gs="git status"
 alias ga="git add"
+alias gac="git add ."
 alias gph="git push"
 alias gpl="git pull"
 alias gfe="git fetch"
 alias gcl="git clone"
 
 # Python
-alias ip="ipython"
 alias p="python3" 
 alias p2="python"
-alias pip="pip3"
-alias pip2="pip"
-alias deac="deactivate"
-alias jp="jupyter notebook"
-
-# iOS
-alias pu="pod update --verbose"
-alias pi="pod install --verbose"
-alias pd="pod deintegrate --verbose"
-alias srm="xcrun simctl uninstall booted"
 
 # Rust
 alias cg="cargo"
