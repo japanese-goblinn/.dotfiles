cf367e74920428f96131386a12c5d21a6e1605c3
japanese-goblinn <cool45akol@gmail.com>
2022-06-05 23:27:59 +0300

    refactor(install): code style

--- shell/install_dotfiles.sh
+++ shell/install_dotfiles.sh
@@ -6,16 +6,12 @@ function dotfiles_install() {
   ln -sF "$DOTFILES_PATH/.zshrc" "$HOME"
   
   (cd $DOTFILES_PATH && git submodule update --init --recursive) 
-  
-  (cd "$DOTFILES_DEPENDECIES_PATH/xcode_theme" && ./install.sh) 
-  
-  # TODO: run config scripts (like setup_sudo*.sh)
-  (cd "$DOTFILES_PATH/config/git" && touch ".github_token" && echo -e "[user]\n\ttoken = " > .github_token)
-  _print_warning "GitHub Token setup needed"
+  (cd "$DOTFILES_DEPENDECIES_PATH/xcode_theme" && "./install.sh") 
 }
 
 function brew_install() {
   _print_info "\nRunning brew install phase..."
+  
   if ! _is_installed "brew"; then
     /bin/bash -c "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh )" 
   fi
@@ -41,6 +37,7 @@ function brew_install() {
   _maybe_brew_cask_install "netnewswire" # Nice open-source RSS client for macOS/iOS
 
   # cli 
+  _maybe_brew_install "robotsandpencils/made/xcodes" # xcode versions manager
   _maybe_brew_install "mas" # download apps from app store 
   _maybe_brew_install "ripgrep" # better grep
   _maybe_brew_install "fd" # better find
@@ -62,12 +59,25 @@ function brew_install() {
   _mas_install "1569600264" # Pandan. Time Tracking app
 }
 
+function tools_install() {
+  _print_info "\nRunning tools install phase..."
+
+  # rust
+  curl --proto '=https' --tlsv1.2 -sSf "https://sh.rustup.rs" | sh
+
+  # xcode
+  xcodes install --latest
+}
+
 function configs_install() {
   _print_info "\nRunning configs install phase..."
+  
   # git
-  ln -sF "$DOTFILES_CONFIG_PATH/git/.gitconfig" ~/.gitconfig 
-  ln -sF "$DOTFILES_CONFIG_PATH/git/.github_token" ~/.github_token 
-  ln -sF "$DOTFILES_CONFIG_PATH/git/.gitignore" ~/.gitignore 
+  (cd "$DOTFILES_CONFIG_PATH/git" && touch ".github_token" && echo -e "[user]\n\ttoken = " > .github_token)
+  _print_warning "GitHub Token setup needed"
+  ln -sF "$DOTFILES_CONFIG_PATH/git/.gitconfig" "$HOME/.gitconfig" 
+  ln -sF "$DOTFILES_CONFIG_PATH/git/.github_token" "$HOME/.github_token"
+  ln -sF "$DOTFILES_CONFIG_PATH/git/.gitignore" "$HOME/.gitignore" 
   
   # run crontab
   crontab "$DOTFILES_CONFIG_PATH/auto_backups/crontab.txt" && crontab -l 
@@ -75,13 +85,13 @@ function configs_install() {
   # vscode
   local VSCODE_PATH="$DOTFILES_CONFIG_PATH/vscode/"
   xargs -L1 code --install-extension < "$VSCODE_PATH/extensions.txt"
-  ln -sF "$VSCODE_PATH/settings.json" ~/Library/Application\ Support/Code/User/settings.json
-  ln -sF "$VSCODE_PATH/keybindings.json" ~/Library/Application\ Support/Code/User/keybindings.json
+  ln -sF "$VSCODE_PATH/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+  ln -sF "$VSCODE_PATH/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
     
   # iterm2
   _print_warning "iTerm needs manual install of config"
   mk "$HOME/Library/Application Support/iTerm2/Scripts/AutoLaunch/"
-  ln -sF "$DOTFILES_CONFIG_PATH/iterm/auto_dark_mode.py" ~/Library/Application\ Support/iTerm2/Scripts/AutoLaunch/auto_dark_mode.py
+  ln -sF "$DOTFILES_CONFIG_PATH/iterm/auto_dark_mode.py" "$HOME/Library/Application Support/iTerm2/Scripts/AutoLaunch/auto_dark_mode.py"
 
   # color picker
   cp "$DOTFILES_CONFIG_PATH/color_picker/Color Picker.app" "/Applications"
@@ -94,15 +104,13 @@ function configs_install() {
   _print_success "Installing CLI tool of Sublime Merge..."
   sudo ln -sF "/Applications/Sublime Merge.app/Contents/SharedSupport/bin/smerge" "/usr/local/bin"
 
-  # rust
-  curl --proto '=https' --tlsv1.2 -sSf "https://sh.rustup.rs" | sh
-
   # lazygit
   _lazygit_config
 }
 
 function additional_setup() { 
   _print_info "\nRunning additional setup phase..."
+  
   _print_warning "'SF Mono' install needed" 
   _print_warning "'raycast config' import needed"
 
@@ -124,6 +132,10 @@ source "$DOTFILES_PATH/shell/functions.sh"
 xcode-select --install 2>/dev/null || _print_warning "Xcode CLI tools already installed"
 dotfiles_install
 brew_install
+tools_install
 configs_install
+
 source "$HOME/.zshrc"
-source "$DOTFILES_PATH/config/macos/defaults.sh"
+
+sudo /bin/zsh "$DOTFILES_PATH/shell/setup_sudo_touch_id.sh"
+/bin/zsh "$DOTFILES_CONFIG_PATH/macos/defaults.sh"
