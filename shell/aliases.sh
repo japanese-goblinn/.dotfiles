52a5c006989292fc0c90a75b45aa425423a94010
Kiryl Harbachonak <cool45akol@gmail.com>
2023-01-11 18:58:18 +0100

    refactor(shell): aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 
-# General
+alias o="open"
+alias oc="open ."
+alias ..="cd .."
+alias ...="cd ../../"
 alias cd="z"
 alias icd="zi"
 alias md="mkdir -p"
@@ -8,6 +11,12 @@ alias mv="mv -iv"
 alias cp="cp -riv"
 alias ls="exa"
 alias ll="exa -la"
+alias g="git"
+alias gcm="git commit -m"
+alias gs="git status"
+alias ga="git add"
+alias gph="git push"
+alias gcl="git clone"
 alias rld="source ~/.zshrc"
 alias f="fork"
 alias mk="make"
@@ -17,26 +26,6 @@ alias find="fd"
 alias grep="rg"
 alias b="bat --theme=\$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo $BAT_DARK_THEME || echo $BAT_LIGHT_THEME)"
 alias speedtest="networkQuality -v"
-
-# Navigation
-alias o="open"
-alias oc="open ."
-alias ..="cd .."
-alias ...="cd ../../"
-
-# git
-alias g="git"
-alias gcm="git commit -m"
-alias gs="git status"
-alias ga="git add"
-alias gph="git push"
-alias gpl="git pull"
-alias gfe="git fetch"
-alias gcl="git clone"
-
-# Rust
-alias cg="cargo"
-
-# Swift
+alias cimg="/Applications/ImageOptim.app/Contents/MacOS/ImageOptim"
 alias spm="swift package"
-
+alias cg="cargo"
