3c379fea1b290a4da80ab822eb71938848cc217d
Kiryl Harbachonak <cool45akol@gmail.com>
2024-02-20 20:00:08 +0100

    fix(shell): mcd function

--- shell/functions.sh
+++ shell/functions.sh
@@ -212,10 +212,13 @@ function tc() {
 function mcd() {
   local dir
   dir="$1"
-  command cd
   if [ -n "$dir" ]; then
     md "$dir"
-    cd "$dir" || return "$ERROR_CODE"
+    __zoxide_z "$dir" || return "$ERROR_CODE"
+  else
+    _print_error "Directory name should not be empty"
+  fi
+}
   else
     _print_error "Directory name should not be empty"
   fi
